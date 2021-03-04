308562616ec1982ada4ef45db1029de331155b62
davidsudm <David.Miranda>
2021-03-04 20:36:38 +0100

    dl2 job script update
    - changing wrong paths

--- lstchain/scripts/bash/run_example/mc_dl1_to_dl2.sh
+++ lstchain/scripts/bash/run_example/mc_dl1_to_dl2.sh
@@ -16,13 +16,13 @@
 # production_name :     just a name label for display output
 
 lstchain_version="0.6.3"
-path_to_lstchain="/home/david.miranda/software/cta-lstchain_v.${lstchain_version}"
+path_to_lstchain="/home/david.miranda/software/cta-sipm-lstchain_v.${lstchain_version}"
 cluster="itcluster"
 rf_type="standard"
 
 percent_event_train=(30 50)
-ARRAY_INTENSITY=(100 200)
-ARRAY_LEAKAGE=(0.0 0.2)
+ARRAY_INTENSITY=(0 100 200)
+ARRAY_LEAKAGE=(0.0 0.25 0.50 0.75 1.0)
 
 for percentage in "${percent_event_train[@]}"; do
 
@@ -32,26 +32,25 @@ for percentage in "${percent_event_train[@]}"; do
 
   working_dir="${path_to_lstchain}/lstchain/scripts/bash/mc_dl1_to_dl2/"
 
-  for folder in "mono-lst-sipm-pmma-3ns" "mono-lst-sipm-pmma-5ns" "mono-lst-sipm-pmma-7ns"; do
-#  for folder in "mono-lst-sipm-borofloat-3ns" "mono-lst-sipm-borofloat-5ns" "mono-lst-sipm-borofloat-7ns"; do
-#  for folder in "tag_nominal_LST_09_2020_v2"; do
+  for folder in "tag_nominal_LST_09_2020_v2" "mono-lst-sipm-pmma-3ns" "mono-lst-sipm-pmma-5ns" "mono-lst-sipm-pmma-7ns" "mono-lst-sipm-borofloat-3ns" "mono-lst-sipm-borofloat-5ns" "mono-lst-sipm-borofloat-7ns"; do
     for leakage in "${ARRAY_LEAKAGE[@]}"; do
       for intensity in "${ARRAY_INTENSITY[@]}"; do
 
         if [[ "${folder}" == "tag_nominal_LST_09_2020_v2" ]]; then
-          json_config_file="/fefs/home/david.miranda/software/cta-lstchain_v.${lstchain_version}/lstchain/data/pmt_cam.json"
+          json_config_file="/fefs/home/david.miranda/software/cta-sipm-lstchain_v.${lstchain_version}/lstchain/data/pmt_cam.json"
         else
-          json_config_file="/fefs/home/david.miranda/software/cta-lstchain_v.${lstchain_version}/lstchain/data/sipm_cam.json"
+          json_config_file="/fefs/home/david.miranda/software/cta-sipm-lstchain_v.${lstchain_version}/lstchain/data/sipm_cam.json"
         fi
 
         path_to_models="/fefs/aswg/workspace/david.miranda/data/prod5/rf/lstchain_v.${lstchain_version}/${folder}/${split_tag}/${rf_type}/intensity_${intensity}_leakage_${leakage}/"
-        input_folder="/fefs/aswg/workspace/david.miranda/data/prod5/dl1_merge/lstchain_v.${lstchain_version}/${folder}/${split_tag}/${rf_type}/intensity_${intensity}_leakage_${leakage}/"
+        input_folder="/fefs/aswg/workspace/david.miranda/data/prod5/dl1_merged/lstchain_v.${lstchain_version}/${folder}/${split_tag}/"
         output_folder="/fefs/aswg/workspace/david.miranda/data/prod5/dl2/lstchain_v.${lstchain_version}/${folder}/${split_tag}/${rf_type}/intensity_${intensity}_leakage_${leakage}/"
         production_name="Prod5_${folder}_with_lstchain_v.${lstchain_version}_leak_${leakage}_intensity_${intensity}"
 
         bash ${working_dir}/mc_dl1_to_dl2.sh "${input_folder}" "${output_folder}" "${path_to_models}" "${json_config_file}" "${intensity}" "${leakage}" "${production_name}" "${working_dir}" "${cluster}"
         sleep 0.1
 
+        echo ""
         echo ${input_folder}
         echo ${output_folder}
         echo ${path_to_models}
@@ -62,6 +61,7 @@ for percentage in "${percent_event_train[@]}"; do
         echo ${working_dir}
         echo ${cluster}
 
+      done
     done
   done
 done
